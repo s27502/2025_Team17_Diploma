6c9badb98a9a26e87984d120556a702d23d8e0a8
s27502 <161813707+s27502@users.noreply.github.com>
2025-11-28 16:06:43 +0100

    Update Parenting for coinFactory

--- Assets/Scripts/Interactables/CoinFactory.cs
+++ Assets/Scripts/Interactables/CoinFactory.cs
@@ -9,7 +9,7 @@ public class CoinFactory : MonoBehaviour
 
     
     
-    public void SpawnCoins(Vector3 pos, int totalValue)
+    public void SpawnCoins(Vector3 pos, int totalValue, GameObject parent)
     {
         int smallCoins = 0;
         int bigCoins = 0;
@@ -19,7 +19,7 @@ public class CoinFactory : MonoBehaviour
             rest = totalValue - 5 * (totalValue / 5);
             for (int i = 0; i < totalValue/5; i++)
             {
-                InstantiateAndLaunch(bigCoinPrefab, pos);
+                InstantiateAndLaunch(bigCoinPrefab, pos, parent);
                 bigCoins += 1;
             }
 
@@ -33,16 +33,17 @@ public class CoinFactory : MonoBehaviour
         {
             for (int i = 0; i < rest; i++)
             {
-                InstantiateAndLaunch(smallCoinPrefab, pos);
+                InstantiateAndLaunch(smallCoinPrefab, pos, parent);
                 smallCoins += 1;
             }
         }
     }
 
-    private void InstantiateAndLaunch(GameObject prefab, Vector3 pos)
+    private void InstantiateAndLaunch(GameObject prefab, Vector3 pos, GameObject parent)
     {
         GameObject coin = Instantiate(prefab, pos, Quaternion.identity);
-
+        coin.transform.SetParent(parent.transform);
+        
         Rigidbody2D rb = coin.GetComponent<Rigidbody2D>();
         rb.gravityScale = 0f; 
         rb.drag = 3f;
