cdc6ed3ceb8efbb2fd25329f59c8c8f26f757c27
s27466 <161813484+s27466@users.noreply.github.com>
2025-11-29 18:07:31 +0100

    damage popup na ekranie

--- Assets/Scripts/ObjectPooling/Projectiles/PlayerProjectile.cs
+++ Assets/Scripts/ObjectPooling/Projectiles/PlayerProjectile.cs
@@ -1,14 +1,18 @@
-﻿using Enemies;
+﻿using UnityEngine;
+using Enemies;
 using Managers;
-using UnityEngine;
 using Player;
 
 public class PlayerProjectile : ProjectileBase
 {
+    [SerializeField] private GameObject damageTextPrefab;
+    [SerializeField] private float damageTextRange = 0.5f;
+
     protected override void Awake()
     {
         base.Awake();
-        damage = ServiceLocator.Instance.GetService<PlayerStatManager>().GetPlayerStats().GetDmg();
+        damage = ServiceLocator.Instance.GetService<PlayerStatManager>()
+            .GetPlayerStats().GetDmg();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -16,15 +20,37 @@ public class PlayerProjectile : ProjectileBase
         if (other.CompareTag("Player"))
             return;
 
-
         if (other.CompareTag("Enemy"))
         {
             var enemyStats = other.GetComponent<EnemyStats>();
-            enemyStats?.ModifyHp(-damage);
+            if (enemyStats != null)
+            {
+                enemyStats.ModifyHp(-damage);
+                ShowDamageText(other.transform.position);
+            }
             _pool?.ReleaseObject(this);
             return;
         }
-        
+
         _pool?.ReleaseObject(this);
+    }
+
+    private void ShowDamageText(Vector3 enemyPos)
+    {
+        if (damageTextPrefab == null)
+            return;
+
+        Vector3 randomOffset = new Vector3(
+            Random.Range(-damageTextRange, damageTextRange),
+            Random.Range(-damageTextRange, damageTextRange),
+            0f
+        );
+
+        GameObject textObj = Instantiate(damageTextPrefab, enemyPos + randomOffset, Quaternion.identity);
+        var dmgPopup = textObj.GetComponent<DamagePopup>();
+        if (dmgPopup != null)
+        {
+            dmgPopup.Setup(damage);
+        }
     }
 }
