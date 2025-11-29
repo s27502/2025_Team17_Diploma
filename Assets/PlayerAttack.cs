942f9f145614682448ea2baf97e20352a1cbb072
s27466 <161813484+s27466@users.noreply.github.com>
2025-11-29 16:41:40 +0100

    podstawy pod atak gracza

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectile;
+    private void Start()
+    {
+        //getprojectile
+        throw new NotImplementedException();
+    }
+
+    private void FixedUpdate()
+    {
+        throw new NotImplementedException();
+    }
+}
