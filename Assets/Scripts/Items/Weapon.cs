620635c1120e8492854eac7c4da786ab89a2f708
s27502 <161813707+s27502@users.noreply.github.com>
2025-11-29 15:00:55 +0100

    Bug fix Updated Bee Scene

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -50,14 +50,14 @@ namespace Items
         {
             List<float> stats = item.GetStats();
             
-            if (stats[2] != 0)
+            if (stats[0] != 0)
             {
-                _stats.ModifyDmg((int)stats[2]);
+                _stats.ModifyDmg((int)stats[0]);
             }
 
-            if (stats[3] != 0)
+            if (stats[1] != 0)
             {
-                _stats.ModifyAttackSpeed(stats[3]);
+                _stats.ModifyAttackSpeed(stats[1]);
             }
         }
 
