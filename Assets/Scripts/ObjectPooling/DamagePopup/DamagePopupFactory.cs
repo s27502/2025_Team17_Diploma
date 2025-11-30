817086000cdfe905bdee022b92ecd2dbed1270c9
s27466 <161813484+s27466@users.noreply.github.com>
2025-11-30 18:33:38 +0100

    damage popup zamieniony na object pooling

---
+++ Assets/Scripts/ObjectPooling/DamagePopup/DamagePopupFactory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DamagePopupFactory : IObjectFactory
+{
+    private GameObject _prefab;
+
+    public DamagePopupFactory(GameObject prefab)
+    {
+        _prefab = prefab;
+    }
+
+    public IPoolableObject CreateNew()
+    {
+        GameObject obj = Object.Instantiate(_prefab);
+        var popup = obj.GetComponent<DamagePopup>();
+        return popup;
+    }
+}
