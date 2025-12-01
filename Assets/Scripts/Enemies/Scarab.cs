c69bdeba6f014bbed8170bece4e41db7004c5d3b
s27466 <161813484+s27466@users.noreply.github.com>
2025-12-01 14:12:03 +0100

    scarab

---
+++ Assets/Scripts/Enemies/Scarab.cs
@@ -0,0 +1,15 @@
+﻿namespace Enemies
+{
+    public class Scarab : Enemy
+    {
+        protected override void Attack()
+        {
+            MoveToPlayer();
+        }
+
+        protected override void Idle()
+        {
+            base.Idle();
+        }
+    }
+}
