b6dabd8c54912f30013d0714815de62469c76eed
s27502 <161813707+s27502@users.noreply.github.com>
2025-12-03 19:00:13 +0100

    Added Basic Stats for future status effects

---
+++ Assets/Scripts/StatSystem/Stats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace StatSystem
+{
+    public class Stats : MonoBehaviour
+    {
+        [SerializeField] protected int _hp;
+        [SerializeField] protected int _maxHp;
+    }
+}
