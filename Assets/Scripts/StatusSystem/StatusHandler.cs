240d293419320d0607dd6341376726c26611ebaf
s27466 <161813484+s27466@users.noreply.github.com>
2025-12-04 21:09:44 +0100

    stats początek

---
+++ Assets/Scripts/StatusSystem/StatusHandler.cs
@@ -0,0 +1,12 @@
+﻿using StatSystem;
+using UnityEngine;
+
+namespace StatusSystem
+{
+    public class StatusHandler : MonoBehaviour
+    {
+        [SerializeField] private Stats _stats;
+        private bool _poisoned;
+        private int _poisonDmg;
+    }
+}
