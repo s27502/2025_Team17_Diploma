2b1909f89d4ef79969f42edcf0802c78336f84c4
s27466 <161813484+s27466@users.noreply.github.com>
2025-12-06 22:48:03 +0100

    cutscenka działa

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textComponent;
+    [SerializeField] private List<string> lines; 
+    [SerializeField] private float textSpeed;
+
+    protected int index;
+    
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+    
+    protected virtual void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Count - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    protected virtual void EndDialogue()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/CutsceneImage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+
+    [CreateAssetMenu(menuName = "Cutscenes/Cutscene Images")]
+    public class CutsceneImage : ScriptableObject
+    {
+        public int index;
+        public Sprite sprite;
+    }
+}
