99d13e77dd5f110641987bcf421802cc71468437
s27502 <161813707+s27502@users.noreply.github.com>
2025-12-08 15:00:28 +0100

    Revert "semi fix"
    This reverts commit 5aadbca358d7d9ed091a3b47daf91e0f87556742.

--- Assets/Scripts/Factory/ItemFactory.cs
+++ Assets/Scripts/Factory/ItemFactory.cs
@@ -8,20 +8,19 @@ using Random = System.Random;
 public class ItemFactory : MonoBehaviour
 {
     [SerializeField] protected List<Item> items = new List<Item>();
-    [SerializeField] protected GameObject itemParent;
-
-    private void Start()
+    private void Awake()
     {
         SpawnItem();
     }
+
 
     protected virtual void SpawnItem()
     {
         var rnd = new Random();
         var item = items[rnd.Next(0, items.Count)];
         item.SetIsShop(false);
-        var spawned = Instantiate(item.gameObject, itemParent.gameObject.transform);
-        spawned.transform.position = transform.position;
+        Instantiate(item.gameObject, gameObject.transform.position, Quaternion.identity);
+        item.transform.SetParent(gameObject.transform.parent);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Factory/ShopItemFactory.cs
+++ Assets/Scripts/Factory/ShopItemFactory.cs
@@ -16,14 +16,14 @@ public class ShopItemFactory : ItemFactory
         Item item = items[i];
         item.SetIsShop(true);
         
-        var spawned = Instantiate(item.gameObject, itemParent.gameObject.transform);
-        spawned.transform.position = transform.position;
+        Instantiate(item.gameObject, empty.transform.position, empty.transform.rotation);
         Destroy(empty);
         
-        // item.transform.position = transform.position;
-        // item.transform.SetParent(itemParent.gameObject.transform);
+        item.transform.position = transform.position;
+        item.transform.SetParent(gameObject.transform.parent);
         item.SetPrice(_price);
         _price.GetComponent<TextMeshPro>().text = item.GetItemPrice().ToString();
         _price.SetActive(true);
+        Destroy(gameObject);
     }
 }
