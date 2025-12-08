5ef36ef6bcf6be37affc05d57440dcac88aed793
s27466 <161813484+s27466@users.noreply.github.com>
2025-12-08 11:42:58 +0100

    deathscreen improved

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -21,13 +21,6 @@ namespace Player
             
             _deathScreenInstance = Instantiate(_deathScreen);
             
-            Canvas canvas = _deathScreenInstance.GetComponentInChildren<Canvas>();
-            Camera cam = Camera.main;
-            if (cam != null)
-            {
-                canvas.worldCamera = cam;
-            }
-
             yield return new WaitForSecondsRealtime(2f);
 
             Destroy(gameObject);
