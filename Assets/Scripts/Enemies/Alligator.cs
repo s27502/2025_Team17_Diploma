8ab69b09ee06de79cede7302f1b3edb390a82b4a
s27466 <161813484+s27466@users.noreply.github.com>
2025-12-10 18:16:32 +0100

    idk

--- Assets/Scripts/Enemies/Alligator.cs
+++ Assets/Scripts/Enemies/Alligator.cs
@@ -7,13 +7,15 @@ public class Alligator : Enemy
     [SerializeField] private float _windUpTime = 2f;
     [SerializeField] private float _miniChargeCooldown = 0.5f;
     [SerializeField] private float _chargingSpeedMult = 10;
-    
+
     private float _windingUpCounter;
     private bool _charging = false;
     private int _chargesNumber = 0;
     private Vector2 _chargeDirection;
     private float _originalSpeed;
     private float _miniChargeCounter = 0f;
+    
+    private bool _canDetectCollision = true;
 
     protected override void Start()
     {
@@ -63,9 +65,20 @@ public class Alligator : Enemy
         _charging = true;
         EnemyStats.SetMovementSpeed(_originalSpeed * _chargingSpeedMult);
         _chargesNumber = RollChargesNumber();
+
         _chargeDirection = (_player.transform.position - transform.position).normalized;
         FlipTo(_chargeDirection.x);
+
         _miniChargeCounter = 0f;
+        
+        StartCoroutine(EnableCollisionNextFrame());
+    }
+
+    private IEnumerator EnableCollisionNextFrame()
+    {
+        _canDetectCollision = false;
+        yield return new WaitForFixedUpdate();
+        _canDetectCollision = true;
     }
 
     private int RollChargesNumber()
@@ -75,7 +88,11 @@ public class Alligator : Enemy
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if ((!other.gameObject.CompareTag("Obstacle") && !other.gameObject.CompareTag("Player")) || !_charging) return;
+        if (!_canDetectCollision) return;
+
+        if (!_charging) return;
+        if (!other.gameObject.CompareTag("Obstacle") && !other.gameObject.CompareTag("Player")) return;
+        
         _chargesNumber--;
         _chargeDirection = (_player.transform.position - transform.position).normalized;
         _miniChargeCounter = _miniChargeCooldown;
