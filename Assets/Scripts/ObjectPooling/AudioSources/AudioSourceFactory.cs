06d41c5cd8f4cf1821a307bdb3f4d672212a5504
s27466 <161813484+s27466@users.noreply.github.com>
2025-12-14 18:57:01 +0100

    audio manager

---
+++ Assets/Scripts/ObjectPooling/AudioSources/AudioSourceFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ObjectPooling.AudioSources
+{
+    public class AudioSourceFactory : IObjectFactory
+    {
+        private GameObject _prefab;
+
+        public AudioSourceFactory(GameObject prefab)
+        {
+            _prefab = prefab;
+        }
+
+        public IPoolableObject CreateNew()
+        {
+            GameObject obj = Object.Instantiate(_prefab);
+            var audioSource = obj.GetComponent<PoolableAudioSource>();
+            return audioSource;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectPooling/AudioSources/PoolableAudioSource.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using Player;
+using UnityEngine;
+
+namespace ObjectPooling.AudioSources
+{
+    public class PoolableAudioSource : MonoBehaviour, IPoolableObject
+    {
+        [SerializeField] private AudioSource _audioSource;
+        
+        private IObjectPool _pool;
+        
+        public void SetPool(IObjectPool pool) => _pool = pool;
+        
+        public void Spawn(Vector2 position, Vector2 direction)
+        {
+            transform.position = position;
+            gameObject.SetActive(true);
+        }
+        
+        public void Spawn(Transform parent, Vector2 position)
+        {
+            transform.SetParent(parent, false);
+            transform.position = position;
+            gameObject.SetActive(true);
+        }
+
+
+        public void Despawn()
+        {
+            gameObject.SetActive(false);
+            
+        }
+
+        public void PlayClip(AudioClip clip)
+        {
+            StartCoroutine(PlayClipCoroutine(clip));
+        }
+
+        private IEnumerator PlayClipCoroutine(AudioClip clip)
+        {
+            _audioSource.clip = clip;
+            _audioSource.Play();
+            
+            yield return new WaitWhile(() => _audioSource.isPlaying);
+            
+            _pool?.ReleaseObject(this);
+        }
+
+    }
+}
