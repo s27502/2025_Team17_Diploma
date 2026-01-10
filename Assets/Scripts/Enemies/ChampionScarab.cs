57df4cb7f1cb8c77de13a7067ec5f27893584939
s27466 <161813484+s27466@users.noreply.github.com>
2026-01-10 19:39:04 +0100

    champion scarab done

--- Assets/Scripts/Enemies/ChampionScarab.cs
+++ Assets/Scripts/Enemies/ChampionScarab.cs
@@ -5,24 +5,35 @@ namespace Enemies
 {
     public class ChampionScarab : Scarab
     {
-        [SerializeField] private float _shootDelay = 1f;
         private float _shootTimer;
+        private Vector2 _shootingDir;
         protected override void Attack()
         {
             base.Attack();
             
             _shootTimer += Time.fixedDeltaTime;
 
-            if (_shootTimer >= _shootDelay)
-            {
-                _shootTimer = 0f;
-                Shoot();
-            }
+            if (!(_shootTimer >= EnemyStats.GetFireRate())) return;
+            _shootTimer = 0f;
+            Shoot();
         }
 
         private void Shoot()
         {
+            if (_player)
+            {
+                _shootingDir = (_player.transform.position - transform.position).normalized;
+            }
+            projectileFactory.Shoot(Rotate(_shootingDir,-40));
+            projectileFactory.Shoot(_shootingDir);
+            projectileFactory.Shoot(Rotate(_shootingDir,40));
             
+            
+        }
+        
+        private Vector2 Rotate(Vector2 v, float angle)
+        {
+            return Quaternion.Euler(0, 0, angle) * v;
         }
     }
 }
