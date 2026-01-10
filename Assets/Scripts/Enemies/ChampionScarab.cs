7cee6dc5693409b87da78bf0ec1c636c18a8e2f1
s27466 <161813484+s27466@users.noreply.github.com>
2026-01-10 19:29:25 +0100

    champion scarab poczatek

---
+++ Assets/Scripts/Enemies/ChampionScarab.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class ChampionScarab : Scarab
+    {
+        [SerializeField] private float _shootDelay = 1f;
+        private float _shootTimer;
+        protected override void Attack()
+        {
+            base.Attack();
+            
+            _shootTimer += Time.fixedDeltaTime;
+
+            if (_shootTimer >= _shootDelay)
+            {
+                _shootTimer = 0f;
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            
+        }
+    }
+}
