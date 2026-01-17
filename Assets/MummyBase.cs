bbedeaf45a74b234d4530066c782ccd68b0d7ad2
s27466 <161813484+s27466@users.noreply.github.com>
2026-01-17 17:24:50 +0100

    mummies finished

--- Assets/MummyBase.cs
+++ Assets/MummyBase.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MummyBase : Enemy
 {
     [SerializeField] private GameObject mummyPile;
+    private float _shootCounter;
 
     public override void Die()
     {
@@ -17,5 +18,53 @@ public class MummyBase : Enemy
     public void Respawn()
     {
         EnemyStats.ModifyHp(EnemyStats.GetMaxHp()/2);
+    }
+
+    protected override void Attack()
+    {
+        CrossMove();
+        Shoot8();
+    }
+    
+    private void Shoot8()
+    {
+        if (_shootCounter <= EnemyStats.GetFireRate())
+        {
+            _shootCounter += Time.fixedDeltaTime;
+        }
+        else
+        {
+            ShootCross();
+            ShootX();
+            _shootCounter = 0f;
+        }
+    }
+    
+    private void ShootCross()
+    {
+        Vector2[] dirs =
+        {
+            Vector2.up,
+            Vector2.down,
+            Vector2.left,
+            Vector2.right
+        };
+
+        foreach (var dir in dirs)
+            projectileFactory.Shoot(dir);
+    }
+
+    private void ShootX()
+    {
+        Vector2[] dirs =
+        {
+            new Vector2(1, 1).normalized,
+            new Vector2(-1, 1).normalized,
+            new Vector2(1, -1).normalized,
+            new Vector2(-1, -1).normalized
+        };
+
+        foreach (var dir in dirs)
+            projectileFactory.Shoot(dir);
     }
 }
